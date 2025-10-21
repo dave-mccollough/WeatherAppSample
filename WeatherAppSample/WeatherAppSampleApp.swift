17df9281dd131b5325d923aa2dceb95e684a00e9
Dave McCollough <davemccollough@gmail.com>
2025-10-21 06:59:31 -0600

    Initial Commit

---
+++ WeatherAppSample/WeatherAppSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSampleApp.swift
+//  WeatherAppSample
+//
+//  Created by Dave McCollough on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
